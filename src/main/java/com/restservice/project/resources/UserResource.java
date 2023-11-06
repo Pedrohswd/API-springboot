30aa1eecd535c063e7c56dbad85c1e4b29832244
Pedro Henrique <pedrohsfwd@gmail.com>
2023-11-06 15:49:41 -0300

    User entity and resource

---
+++ src/main/java/com/restservice/project/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.restservice.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restservice.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Pedro", "pedro@email.com", "0021311231", "pedron");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
