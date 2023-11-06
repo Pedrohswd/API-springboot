56ad43975622708182cba98d8f2efa6438db9b30
Pedro Henrique <pedrohsfwd@gmail.com>
2023-11-06 16:16:07 -0300

    H2 database, test profile, JPA

--- src/main/java/com/restservice/project/resources/UserResource.java
+++ src/main/java/com/restservice/project/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Pedro", "pedro@email.com", "0021311231", "pedron");
+ 		User u = new User(1L, "Pedro", "pedro@email.com", "0021311231", "pedron");
 		return ResponseEntity.ok().body(u);
 		
 	}
