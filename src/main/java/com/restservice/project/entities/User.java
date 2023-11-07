3f7b07483e3d7598f61e1c1c380ce1a3d28af15e
Pedro Henrique <pedrohsfwd@gmail.com>
2023-11-07 14:21:18 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/restservice/project/entities/User.java
+++ src/main/java/com/restservice/project/entities/User.java
@@ -1,19 +1,24 @@
 package com.restservice.project.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -21,9 +26,13 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(long id, String name, String email, String phone, String password) {
@@ -35,7 +44,7 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public long getId() {	
+	public long getId() {
 		return id;
 	}
 
@@ -75,6 +84,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +104,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return id == other.id;
 	}
-	
-	
+
 }
